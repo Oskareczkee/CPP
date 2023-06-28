3a50d465e4f877249c7859bdfdc20d4fa0658c23
Oskar Niedźwiedzki <80743794+Oskareczkee@users.noreply.github.com>
2023-06-28 19:26:06 +0200

    Added new Files

---
+++ Book Excersises/Classes/Cykl Życia Klasy.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "vectorinho.h"
+
+using namespace std;
+
+
+//byczq, operator "=" narazie nie dziala, chuj wie, pewnie kiedys to naprawie, ale nie teraz, bo jestem zmeczony, aha i ta metoda pod kopiowaniem to nawet nie wiem co robi xDDDD
+
+int main()
+{
+    //przyklad uzycia
+    vector contact(5);
+    vector siema(contact);
+    siema = contact;
+    contact.ustaw(2, 5);
+    contact.dodaj(15);
+    contact.wstaw(30);
+    cout << contact.pobierz(0);
+    cout << "duppa";
+}
